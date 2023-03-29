f17c71ef7b190d45e84c5d0e2b8016d54305cfcf
Martin Schädler <github@alien.de>
2023-03-29 11:40:36 +0200

    First modification

---
+++ README.md
@@ -0,0 +1,15 @@
+# INSTALLATION
+
+npm install
+
+(You have had to need NodeJs installed on your mac)
+
+# CONFIGURATION
+
+Open the .env file and change it.
+Add your OPEN-API-KEY there!
+
+# RUN
+
+node script.js
+
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+import fs from "fs"
 import { config } from "dotenv"
 config()
+import { exec } from "child_process"
 
 import { Configuration, OpenAIApi } from "openai"
 import readline from "readline"
@@ -15,12 +18,62 @@ const userInterface = readline.createInterface({
   output: process.stdout,
 })
 
+// array of messages, will will grow by user input and AI response
+const messages = []
+
+// load file with prompt from filesystem, that is in the same folder as this script
+let prompt = fs.readFileSync(process.env.PROMPT_FILE, "utf8")
+prompt = prompt
+  .replace('USER_NAME', process.env.USER_NAME)
+  .replace('AI_NAME', process.env.AI_NAME)
+
+messages.push( { role: "user", content: prompt } )
+
 userInterface.prompt()
+
+userInterface.on("close", async () => {
+  console.log("=== Conversation ended ===");
+  const strDate = new Date().toISOString();
+  const fileJson = `conversations/${strDate}.json`
+  await fs.writeFileSync(fileJson, JSON.stringify(messages, null, 2))
+
+  let fullTxt = "";
+  messages.forEach((message) => {
+    if (fullTxt == "") {
+      fullTxt += `${message.content}\n\n`
+
+    } else if (message.role == "user") {
+      fullTxt += `\n\n${process.env.USER_NAME}:\n${message.content}\n\n`
+    } else {
+      fullTxt += `\n\n${process.env.AI_NAME}:\n${message.content}\n\n`
+    }
+  })
+  const fileTxt = `conversations/${strDate}.txt`
+  await fs.writeFileSync(fileTxt, fullTxt)
+
+  console.log(`Conversation saved to\n-> ${fileJson}\n-> ${fileTxt}\n`)
+  process.exit(0)
+})
+
 userInterface.on("line", async input => {
+  messages.push( { role: "user", content: input } )
+
   const response = await openAi.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
+    messages: messages,
   })
-  console.log(response.data.choices[0].message.content)
+
+  let aiTxt = response.data.choices[0].message.content;
+
+  console.log(aiTxt)
+
+  messages.push( { role: "assistant", content: aiTxt } )
+
+  const speak = (text) => {
+    exec(`say -v ${process.env.VOICE} "${text}" &`)
+  }
+
+  speak(response.data.choices[0].message.content)
+
   userInterface.prompt()
 })
